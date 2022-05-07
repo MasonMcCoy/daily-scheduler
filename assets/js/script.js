c67563e1bb85a12a40d5e0b4d37beeb07cfff4d7
Mason McCoy <mmasonmccoy@gmail.com>
2022-05-06 20:58:30 -0500

    Added hover effect on save buttons. Clicking a save button will now store its adjacent timeblock input in Local Storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
-// Sets current day and date
+// DOM variables
 var today = $("#currentDay");
-
-today.text(moment().format("dddd, MMMM Do"));
-
-var currentHour = moment().format("H");
-var workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-
-
-// Adds time blocks
 var blockCon = $("#schedule-blocks");
 
+// Sets current day and date
+today.text(moment().format("dddd, MMMM Do"));
+
+// Current hour using military time
+var currentHour = moment().format("H");
+
+// Array of normal business hours
+var workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+// Adds time blocks that contain the hour, a form with a textarea, and a save icon
 workHours.forEach((hourTxt, i) => {
     var hrRow = $("<div>").addClass("row");
     var hourDiv = $("<div>").addClass("col-2").text(hourTxt).addClass("time-block");
     var schedDiv = $("<div>").addClass("col-8");
-    var schedForm = $("<form>").append($("<textarea>"));
-    var svIcon = $("<i>").addClass("col-2 saveBtn fa-solid fa-floppy-disk fa-2xl"); //fa-solid fa-floppy-disk fa-2xl 
+    var formID = "text-input-" + hourTxt;
+    var schedForm = $("<form>").append($("<textarea>").attr("id", formID));
+    var svIcon = $("<i>").addClass("col-2 saveBtn fa-solid fa-floppy-disk fa-2xl");
 
      if ((i + 9) < currentHour) {
         schedDiv.addClass('past')
@@ -34,35 +37,29 @@ workHours.forEach((hourTxt, i) => {
     blockCon.append(hrRow);
 });
 
-// for (var i = 9; i < 18; i++) {
-    // var hrRow = $("<div>").addClass("row");
-    // var hour = $("<div>").addClass("col-2");
-    // var schedDiv = $("<div>").addClass("col-8 time-block");
-    // var schedForm = $("<form>").append($("<textarea>").attr("rows", "4").attr("cols", "50"));
-    // var svIcon = $("<i>").addClass("col-2 fa-solid fa-floppy-disk fa-2xl");
+// Stores user input in Local Storage
+function saveTxt(event) {
+    var userClick = event.target;
 
+    if (userClick.tagName != "I") {
+        return
+    }
 
-//     if (i > 12) {
-//         hour.text([i] - 12 + "PM");
-//     } else if (i < 12) {
-//         hour.text([i] + "AM");
-//     } else {
-//         hour.text([i] + "PM");
+    var userInput = userClick.previousElementSibling.firstElementChild.firstElementChild;
+    console.log(userInput)
+    localStorage.setItem(userInput.id, userInput.value);
+}
+
+blockCon.on("click", saveTxt);
+
+// function updateTxt(event) {
+//     var userChange = event.target;
+
+//     if (userChange.tagName != "TEXTAREA") {
+//         return
 //     }
-
-//     schedForm.addClass("description");
-
-//     schedDiv.append(schedForm);
-
-//     hrRow.append(hour);
-//     hrRow.append(schedDiv);
-//     hrRow.append(svIcon);
-
-//     blockCon.append(hrRow);
+    
+//     console.log(userChange.value);
 // }
 
-//     i < currentHour
-//     ? schedDiv.addClass('past')
-//     : i === currentHour
-//     ? schedDiv.addClass('present')
-//     : schedDiv.addClass('future')
+// blockCon.on("change", updateTxt)
